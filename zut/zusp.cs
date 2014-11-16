fd7b7b6756ac2c46908b8b4a5604c98ee914d900
ZengXiao <zuoanqh@live.com>
2014-11-16 09:24:39 -0500

    加入部分方法签名。

--- zut/zusp.cs
+++ zut/zusp.cs
@@ -23,6 +23,7 @@ namespace cn.zuoanqh.open.zut
       if (s.Length <= n) return s;
       return s.Substring(0, n);
     }
+
     /// <summary>
     /// 
     /// </summary>
@@ -37,18 +38,62 @@ namespace cn.zuoanqh.open.zut
     }
 
     /// <summary>
+    /// Remove the first n charecters from the given string. 
+    /// </summary>
+    /// <param name="s"></param>
+    /// <param name="n"></param>
+    /// <returns>The remaining charecters. Empty if given string is not long enough.</returns>
+    public static string Drop(string s, int n)
+    {
+      return "";
+    }
+    /// <summary>
+    /// Remove the last n charecters from the given string. 
+    /// </summary>
+    /// <param name="s"></param>
+    /// <param name="n"></param>
+    /// <returns>The remaining charecters. Empty if given string is not long enough.</returns>
+    public static string DropLast(string s, int n)
+    {
+      return "";
+    }
+
+    /// <summary>
+    /// Chop the given string into two part, where the first part have n charecters.
+    /// The two part are equavilent to Left(s, n) and Drop(s, n) respectively.
+    /// </summary>
+    /// <param name="s"></param>
+    /// <param name="n"></param>
+    /// <returns>The two parts. The entire string then empty string if given string is not long enough.</returns>
+    public static Twin<string> ChopLeft(string s, int n)
+    {
+      return null;
+    }
+    /// <summary>
+    /// Chop the given string into two part, where the first part have n charecters.
+    /// The two part are equavilent to DropLast(s, n) and Right(s, n) respectively.
+    /// </summary>
+    /// <param name="s"></param>
+    /// <param name="n"></param>
+    /// <returns>The two parts. Empty string then the entire string if given string is not long enough.</returns>
+    public static Twin<string> ChopRight(string s, int n)
+    {
+      return null;
+    }
+
+    /// <summary>
     /// Chop down the part of given string before the first occourance of given separator.
     /// </summary>
     /// <param name="s"></param>
     /// <param name="Separator"></param>
     /// <returns>A pair of strings in their origional order.</returns>
-    public static Pair<string, string> ChopHead(string s, string Separator)
+    public static Twin<string> CutFirst(string s, string Separator)
     {
       if (s == null) throw new ArgumentNullException("s");
       if (Separator == null) throw new ArgumentNullException("Separator");
       int loc = s.IndexOf(Separator);
       if (loc < 0) throw new ArgumentException("Given string does not contain Separator string.");
-      return new Pair<string, string>(s.Substring(0, loc), s.Substring(loc + Separator.Length));
+      return new Twin<string>(s.Substring(0, loc), s.Substring(loc + Separator.Length));
     }
 
     /// <summary>
@@ -57,13 +102,13 @@ namespace cn.zuoanqh.open.zut
     /// <param name="s"></param>
     /// <param name="Separator"></param>
     /// <returns>A pair of strings in their origional order, without the separator.</returns>
-    public static Pair<string, string> ChopTail(string s, string Separator)
+    public static Twin<string> CutLast(string s, string Separator)
     {
       if (s == null) throw new ArgumentNullException("s");
       if (Separator == null) throw new ArgumentNullException("Separator");
       int loc = s.LastIndexOf(Separator);
       if (loc < 0) throw new ArgumentException("Given string does not contain given Separator.");
-      return new Pair<string, string>(s.Substring(0, loc), s.Substring(loc + Separator.Length));
+      return new Twin<string>(s.Substring(0, loc), s.Substring(loc + Separator.Length));
     }
 
     /// <summary>
